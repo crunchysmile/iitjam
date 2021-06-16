000c9436957a1ce825016dc0416c5e768f677d68
crunchysmile <74782403+crunchysmile@users.noreply.github.com>
2021-06-16 10:20:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-document.getElementsByClassName(".contents-in-detail").querySelectorAll(a).classList.add(".flx-clm");
+   
+           function makeCros(x){
+            x.classList.toggle("change");
+        }
+       
+
+       var cross = document.getElementById("menu") ;
+
+       cross.addEventListener("click", function showMenus(){
+           document.getElementById("menu-contents").classList.toggle("showMenus");
+       }) ;
+
+// //  Automatic hoover active nav li
+//  const topStudies = document.querySelectorAll(".top") ;
+//  const navli = document.querySelectorAll(".menu-contents ul li");
+
+// //  event listener starting 
+// window.addEventListener("scroll", ()=>{
+//   let current = "";
+
+//   topStudies.forEach( div =>{
+//      const studiesTop = div.offsetTop ;
+//     //  console.log(studiesTop);
+//      const studiesHeight = div.clientHeight;
+//     //  console.log(studiesHeight) ;
+//     if(pageYOffset >= studiesTop ){
+//       current = div.getAttribute("id");
+      
+//     }
+//   })
+
+// })
